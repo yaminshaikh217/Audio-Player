b7e0c1881123998431baa21a2cea4e9ab20a6b26
Yamin Shaikh <yaminshaikh.ys47@gmail.com>
2021-10-19 11:50:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const sounds = ['prank', 'gun', 'headshot', 'surprise', 'sad', 'mine'];
+
+sounds.forEach((currentValue) => {
+
+    let btn = document.createElement('button');
+    btn.classList.add("btn");
+    btn.innerText = currentValue;
+    document.getElementById("root").appendChild(btn);
+
+    btn.addEventListener("click", () => {
+
+        stopAudio();  // to stop previous audio
+
+
+        document.getElementById(currentValue).play()
+
+
+    })
+
+})
+
+function stopAudio() {
+
+    sounds.forEach((currentValue) => {
+
+        const audios = document.getElementById(currentValue)
+        audios.pause()
+        audios.currentTime = 0  // its resetting the audio
+    })
+
+}
